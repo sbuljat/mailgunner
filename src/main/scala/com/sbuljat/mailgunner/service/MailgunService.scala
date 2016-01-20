c2f5db23596d238305de2d1ef1c16b5116dbf627
sbuljat <sbuljat@gmail.com>
2016-01-20 15:14:37 +0100

    Add logger

--- src/main/scala/com/sbuljat/mailgunner/service/MailgunService.scala
+++ src/main/scala/com/sbuljat/mailgunner/service/MailgunService.scala
@@ -1,6 +1,7 @@
 package com.sbuljat.mailgunner.service
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import spray.http._
 import spray.client.pipelining._
 import com.sbuljat.mailgunner.util.ApplicationConfig._
@@ -14,21 +15,22 @@ case class MailgunResponse(success:Boolean, msg:String)
 
 trait MailgunApi {
 
-  def send(to:String, subject:String, text:String):Future[MailgunResponse]
+  def send(to:String, subject:String, html:String):Future[MailgunResponse]
 
 }
 
 class MailgunService extends MailgunApi{
   private implicit val system = ActorSystem("mailgun-client")
   import system.dispatcher
+  val log = Logging(system, getClass)
 
   private val pipeline: HttpRequest => Future[HttpResponse] = ( addCredentials(BasicHttpCredentials("api", Mailgun.apiKey)) ~> sendReceive )
 
-  def send(to:String, subject:String, text:String):Future[MailgunResponse] = {
-
-    val payload = FormData(Seq("from" -> Mailgun.from, "to" -> to, "subject" -> subject, "text" -> text))
+  def send(to:String, subject:String, html:String):Future[MailgunResponse] = {
+    val payload = FormData(Seq("from" -> Mailgun.from, "to" -> to, "subject" -> subject, "html" -> html))
 
     pipeline(Post(Mailgun.endpoint, payload)).map{response =>
+      log.info(s"Message send status to $to => ${response.status.toString()} ${response.entity.asString}")
       MailgunResponse(response.status.isSuccess, response.entity.asString)
     }
   }
--- src/test/scala/com/sbuljat/mailgunner/MailgunServiceSpec.scala
+++ src/test/scala/com/sbuljat/mailgunner/MailgunServiceSpec.scala
@@ -12,11 +12,12 @@ import scala.concurrent.duration._
   * Created by stipe on 20.1.2016..
   */
 class MailgunServiceSpec extends Specification{
+  val BODY_HELLOWORLD = "<html><body><h1>Hello World!</b></body></html>"
 
   "MailgunService" should {
 
     "send message to 'sbuljat@gmail.com' without issues" in {
-      Await.result( new MailgunService().send("sbuljat@gmail.com", "Mailgun TEST", "Ahoooooj!"), atMost = Duration.apply(10, TimeUnit.SECONDS) ).success must beTrue
+      Await.result( new MailgunService().send("sbuljat@gmail.com", "Mailgun TEST", BODY_HELLOWORLD), atMost = Duration.apply(10, TimeUnit.SECONDS) ).success must beTrue
     }
 
   }
